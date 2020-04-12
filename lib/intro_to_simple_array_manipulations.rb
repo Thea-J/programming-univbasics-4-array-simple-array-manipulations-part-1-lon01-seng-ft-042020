4bd45f439589a23d766ec9d1654773f124578d6c
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 12:24:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,6 +4,17 @@ def using_push(array, string)
   array.push(string)
 end
 
+#Adds string to the end of the array
+#Return the updated array
 def using_unshift(array, string)
   array.unshift(string)
 end
+
+#Remove the last element from an array
+#Return the removed element
+def using_pop(array)
+  array.pop
+end
+
+
+
