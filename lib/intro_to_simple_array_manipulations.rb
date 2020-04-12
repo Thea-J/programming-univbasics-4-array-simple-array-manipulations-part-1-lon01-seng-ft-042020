ec3fcf405b4b349157822ffc2d5e32e8c92bdd31
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 12:36:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,14 +17,10 @@ def using_pop(array)
   array.pop
 end
 
-#
+#Remove the last two array items and return them
 def pop_with_args(array,numb = 2)
  array.pop(2)
 end
-
-
-
-
 
 #Remove the 1st element from an array
 #Return the removed element
