2f0f6d6384fb28c95866ebe3b78a4fd6bd8557b9
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 12:39:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+
+#Adds string to the end of the array
+#Return the updated array
+def using_push(array, string)
+  array.push(string)
+end
+
+#Adds string to the end of the array
+#Return the updated array
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#Remove & return the last element from an array
+def using_pop(array)
+  array.pop
+end
+
+#Remove the last two array items and return them
+def pop_with_args(array,numb = 2)
+ array.pop(2)
+end
+
+#Remove & return the 1st element from an array
+def using_shift(array)
+  array.shift
+end
+
+#Remove & return the 1st 2 items from the array
+def shift_with_args(array, number = 2)
+  array.shift(number)
+end
